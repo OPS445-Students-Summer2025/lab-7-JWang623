2c3efdad14ad446f975528b950641a576a723c2b
JunLong Wang <jwang557@myseneca.ca>
2025-07-17 19:50:48 -0400

    Lab7 done.

--- lab7c.py
+++ lab7c.py
@@ -16,19 +16,28 @@ def format_time(t):
 
 def sum_times(t1, t2):
     """Add two time objests and return the sum."""
-    sum = Time(0,0,0)
-    sum.hour = t1.hour + t2.hour
-    sum.minute = t1.minute + t2.minute
-    sum.second = t1.second + t2.second
+    total_sec = time_to_sec(t1) + time_to_sec(t2)
 
-    while sum.second >= 60:
-        sum.second -= 60
-        sum.minute += 1
-    while sum.minute >= 60:
-        sum.minute -= 60
-        sum.hour += 1
+    return sec_to_time(total_sec)
 
-    return sum
+def change_time(time, seconds):
+    total_sec = time_to_sec(time) + seconds
+    return None
+
+def time_to_sec(time):
+    '''convert a time object to a single integer representing the number of seconds from mid-night'''
+    minutes = time.hour * 60 + time.minute
+    seconds = minutes * 60 + time.second
+    return seconds
+
+def sec_to_time(seconds):   
+    '''convert a given number of seconds to a time object in hour,minute,second format'''
+    time = Time()
+    minutes, time.second = divmod(seconds, 60)
+    time.hour, time.minute = divmod(minutes,60)
+    return time
+
+
 
 def valid_time(t):
     """check for the validity of the time object attributes:
